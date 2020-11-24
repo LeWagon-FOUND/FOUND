6460e89e26f03516bfca497ef4660f3bc3a18d92
Olivier <dOtreppe>
2020-11-24 17:11:43 +0100

    PUBLIC and ITEM page design

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,15 +4,9 @@ class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :public ]
   before_action :set_profile, only: [:show, :edit, :update, :destroy, :public]
 
-  def show
-  end
-
   def create
-    url = "http://localhost:3000/public_profile/#{current_user.id}"
     profile = Profile.new(profile_params)
     profile.user = current_user
-    profile.svg = get_svg_code(url)
-    # profile.qrcode.attach(io: svg, filename: 'qrcode.svg', content_type: 'image/svg')
     if profile.save
       redirect_to profile_path(current_user)
     else
@@ -20,8 +14,7 @@ class ProfilesController < ApplicationController
     end
   end
 
-  def update
-  end
+  def update; end
 
   def public
     @user = User.new
@@ -35,16 +28,5 @@ class ProfilesController < ApplicationController
 
   def set_profile
     @profile = User.find(params[:id]).profile
-  end
-
-  def get_svg_code(url)
-    qrcode = RQRCode::QRCode.new(url)
-    svg = qrcode.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 6,
-      standalone: true
-    )
   end
 end
