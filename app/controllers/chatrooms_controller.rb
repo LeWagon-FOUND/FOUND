d570dedb37594f550e41dd97cbd64e19d6ed0ffc
Olivier <dOtreppe>
2020-11-24 12:08:55 +0100

    Item Specific QR Code, Chatcreation

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,13 +1,18 @@
 class ChatroomsController < ApplicationController
-
   def create
-    chatroom = Chatroom.create!(name: "Found your item", finder_user_id: current_user.id, user_id: params[:chatroom][:user_id])
-    redirect_to chatrooms_path(chatroom)
+    if params[:chatroom][:item_id].present?
+      chatroom_name = Item.find(params[:chatroom][:item_id]).name.to_s
+      Chatroom.create!(name: chatroom_name, finder_user_id: current_user.id, user_id: params[:chatroom][:user_id])
+    else
+      chatroom_name = "#{current_user.incomplete_name} - #{User.find(params[:chatroom][:user_id]).incomplete_name}"
+      Chatroom.create!(name: chatroom_name, finder_user_id: current_user.id, user_id: params[:chatroom][:user_id])
+    end
+    redirect_to profile_path
   end
 
   private
 
   def chatroom_params
-    params.require(:chatroom).permit(:user)
+    params.require(:chatroom).permit(:user, :item)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :chatrooms, only: [:new, :create] do
+  resources :chatrooms, only: [:new, :create, :show] do
     resources :messages, only: :create
   end
 
   resource :user do
       resources :items
   end
-  get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  get 'public_profile/:id', to: "pages#public", as: :public_profile
-  get 'team', to: 'pages#team', as: :the_team
-#  get 'generate', to: "pages#generate", as: :generate
+
+  get 'dashboard',                    to: 'pages#dashboard',  as: :dashboard
+  get 'profile',                      to: 'pages#profile',    as: :profile
+  get 'public_profile/:id',           to: "pages#public",     as: :public_profile
+  get 'team',                         to: 'pages#team',       as: :the_team
+  get 'public_profile/item/:id',      to: "pages#item",       as: :item_scanned
 end
