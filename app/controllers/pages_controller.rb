3488265ea4b1395c39612d1f00a2975bd132ba06
Olivier <dOtreppe>
2020-11-24 16:23:30 +0100

    allowing to go to the found item page if not authenticted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home public]
+  skip_before_action :authenticate_user!, only: %i[home public item]
   before_action :set_user, only: %i[dashboard generate]
 
   def home; end
